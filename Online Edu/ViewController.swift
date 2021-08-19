b6afe433c0617413b13f0298fc5c655dca5a8488
Likhit Garimella <likhitgmella@gmail.com>
2021-08-19 21:28:32 +0530

    Initial Commit

---
+++ Online Edu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 19/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
