9063d804ad51dfc7194beeefb01e8866535101b4
Likhit Garimella <likhitgmella@gmail.com>
2021-08-20 19:04:52 +0530

    signup screen

--- Online Edu/SignupViewController.swift
+++ Online Edu/SignupViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class SignupViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // Outlets
+    @IBOutlet var emailTextField: FloatLabelTextField!
+    @IBOutlet var passTextField: FloatLabelTextField!
+    @IBOutlet var confPassTextField: FloatLabelTextField!
+    
+    func UnderlineTextField() {
         
-        
+        emailTextField.setUnderLine()
+        passTextField.setUnderLine()
+        confPassTextField.setUnderLine()
         
     }
     
-}   // #20
+    func LeftPadding() {
+        
+        emailTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: emailTextField.frame.height))
+        emailTextField.leftViewMode = .always
+        passTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: passTextField.frame.height))
+        passTextField.leftViewMode = .always
+        confPassTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: confPassTextField.frame.height))
+        confPassTextField.leftViewMode = .always
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hideKeyboardWhenTappedAround()
+        UnderlineTextField()
+        LeftPadding()
+        
+    }
+    
+}   // #46
