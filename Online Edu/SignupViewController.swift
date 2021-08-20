4060d930df8737862f89802804af51675f1a823b
Likhit Garimella <likhitgmella@gmail.com>
2021-08-20 19:47:18 +0530

    login, signup, launch screens addns

--- Online Edu/SignupViewController.swift
+++ Online Edu/SignupViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 class SignupViewController: UIViewController {
     
     // Outlets
+    @IBOutlet var nameTextField: FloatLabelTextField!
+    @IBOutlet var mobnoTextField: FloatLabelTextField!
     @IBOutlet var emailTextField: FloatLabelTextField!
     @IBOutlet var passTextField: FloatLabelTextField!
     @IBOutlet var confPassTextField: FloatLabelTextField!
     
     func UnderlineTextField() {
         
+        nameTextField.setUnderLine()
+        mobnoTextField.setUnderLine()
         emailTextField.setUnderLine()
         passTextField.setUnderLine()
         confPassTextField.setUnderLine()
@@ -24,6 +28,10 @@ class SignupViewController: UIViewController {
     
     func LeftPadding() {
         
+        nameTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: nameTextField.frame.height))
+        nameTextField.leftViewMode = .always
+        mobnoTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: mobnoTextField.frame.height))
+        mobnoTextField.leftViewMode = .always
         emailTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: emailTextField.frame.height))
         emailTextField.leftViewMode = .always
         passTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: passTextField.frame.height))
@@ -42,4 +50,4 @@ class SignupViewController: UIViewController {
         
     }
     
-}   // #46
+}   // #54
