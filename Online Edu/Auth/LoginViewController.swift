7b4b299f9d46a733f44731619054b8e5db38f73b
Likhit Garimella <likhitgmella@gmail.com>
2021-08-26 03:56:34 +0530

    login vc code @304 done

--- Online Edu/Auth/LoginViewController.swift
+++ Online Edu/Auth/LoginViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 import Toast_Swift
+import MBProgressHUD
 
 class LoginViewController: UIViewController {
     
     // Outlets
     @IBOutlet var emailTextField: FloatLabelTextField!
     @IBOutlet var passTextField: FloatLabelTextField!
+    
+    var reachability: Reachability?
     
     func UnderlineTextField() {
         
@@ -41,8 +44,132 @@ class LoginViewController: UIViewController {
         
     }
     
+    func apiCalling() {
+        
+        do {
+            self.reachability = try Reachability.init()
+        }
+        catch {
+            print("Unable to start notifier")
+        }
+        
+        if ((reachability!.connection) != .unavailable) {
+            
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+            
+            let url = "http://3.108.14.230:3333/api/authentication/login"
+            print(url)
+            
+            let params: [String: Any] = [
+                
+                "email": self.emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines),
+                "password": self.passTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                
+            ]
+            
+            print(params)
+            print("1111--------------------------------1111")
+            
+            AF.request(URL.init(string: url)!, method: .post, parameters: params, encoding: JSONEncoding.default).responseJSON { (response) in
+                
+                print(response.request!)
+                print("2222--------------------------------2222")
+                print(response.result)
+                print("3333--------------------------------3333")
+                print(response.response!)
+                print("4444--------------------------------4444")
+                
+                switch response.result {
+                
+                case .success(let payload):
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    if let x = payload as? Dictionary<String,AnyObject> {
+                        print(x)
+                        print("5555--------------------------------5555")
+                        
+                        let resultValue = x as NSDictionary
+                        // let message = resultValue["status"] as String
+                        guard let message = resultValue["status"] as? String else {return}
+                        // let code = resultValue["code"] as! String
+                        
+                        if message == "SUCCESS" {
+                            
+                            print("6666--------------------------------6666")
+                            
+                            // guard let data = resultValue["data"] as? NSDictionary else {return}
+                            let data = resultValue["data"] as! NSDictionary
+                            // guard let token = resultValue["accessToken"] as? String else {return}
+                            // let token = resultValue["accessToken"] as! String
+                            let token = data["accessToken"] as! String
+                            // guard let userId = data["_id"] as? String else {return}
+                            // let userId = data["_id"] as! String
+                            
+                            print("7777--------------------------------7777")
+                            
+                            UserDefaults.standard.setValue("\(token)", forKey: "ApiToken")
+                            // UserDefaults.standard.setValue(userId, forKey: "UserId")
+                            
+                            print("8888--------------------------------8888")
+                            
+                            self.performSegue(withIdentifier: "loginToHome", sender: self)
+                            
+                            /*
+                            let alertController = UIAlertController(title: "Success!", message: "Welcome", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: nil)
+                            */
+                            
+                        } else {
+                            let alertController = UIAlertController(title: "Oops-1", message: "\(message)", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                
+                case .failure(let error):
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Oops-2", message: "App cannot be connected to the server", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    self.present(alertController, animated: true, completion: nil)
+                
+                }
+                
+            }
+            
+        } else {
+            let alertController = UIAlertController(title: "Oops-3", message: "Please check your Internet", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
     @IBAction func loginSucess(_ sender: UIButton) {
         
+        func isValidEmail(testStr: String) -> Bool {
+            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+            let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+            return emailTest.evaluate(with: testStr)
+        }
+        
+        if (emailTextField.text?.isEmpty == false || passTextField.text?.isEmpty == false) {
+            
+            /// call api
+            apiCalling()
+            
+        } else {
+            let alertController = UIAlertController(title: "Oops", message: "Please enter all the fields", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+        /*
         acceptLogin { (success) in
             
             if success {
@@ -90,9 +217,11 @@ class LoginViewController: UIViewController {
             }
             
         }
+        */
         
     }
     
+    /*
     typealias CompletionHandler = (_ success: Bool) -> Void
     func acceptLogin(completionHandler: @escaping CompletionHandler) {
         
@@ -169,5 +298,6 @@ class LoginViewController: UIViewController {
         }
         
     }
+    */
     
-}   // #174
+}   // #304
