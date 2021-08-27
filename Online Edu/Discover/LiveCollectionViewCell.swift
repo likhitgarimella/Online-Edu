32a8a50e28d64f9a9af54bee9ee792d3ca22f565
Likhit Garimella <likhitgmella@gmail.com>
2021-08-27 16:56:22 +0530

    2 coll cells code

---
+++ Online Edu/Discover/LiveCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LiveCollectionViewCell.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 27/08/21.
+//
+
+import UIKit
+
+class LiveCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var labelOne: UILabel!
+    @IBOutlet var labelTwo: UILabel!
+    @IBOutlet var photoImg: UIImageView!
+    @IBOutlet var bottomView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        photoImg.layer.cornerRadius = 12
+        bottomView.layer.cornerRadius = 12
+        bottomView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
+    }
+    
+}   // #27
