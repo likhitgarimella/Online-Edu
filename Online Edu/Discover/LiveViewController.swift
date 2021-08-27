07645faf0fb1231587f2c83d598d688f96d620e6
Likhit Garimella <likhitgmella@gmail.com>
2021-08-27 17:38:39 +0530

    live vc code @62 static

--- Online Edu/Discover/LiveViewController.swift
+++ Online Edu/Discover/LiveViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class LiveViewController: UIViewController {
     
+    @IBOutlet var liveCollectionView: UICollectionView!
+    
+    var dataInfo: NSMutableArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        liveCollectionView.dataSource = self
+        liveCollectionView.delegate = self
         
     }
     
-}   // #20
+}
+
+extension LiveViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        /// static
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LiveCollectionViewCell", for: indexPath) as! LiveCollectionViewCell
+        
+        return cell
+        
+    }
+    
+}
+
+extension LiveViewController: UICollectionViewDelegateFlowLayout {
+    
+    // spacing b/w rows in coll view
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        /// spacing distance = 2
+        return 8
+    }
+    
+    // spacing b/w columns in coll view
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        /// shrink by 1 point for each cell, so that spacing becomes 2 b/w cell-to-cell
+        return CGSize(width: collectionView.frame.size.width / 3 - 2, height: collectionView.frame.size.width / 3 - 2)
+    }
+    
+}   // #62
