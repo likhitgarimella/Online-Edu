6661378a9a37914bb9313784fe42049fee1e6cf9
Likhit Garimella <likhitgmella@gmail.com>
2021-08-29 04:04:04 +0530

    home screen vc @42; video coll cell @20

--- Online Edu/Tab Bar/HomeScreenViewController.swift
+++ Online Edu/Tab Bar/HomeScreenViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
     
+    private var collectionView: UICollectionView?
+    
+    private var data = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.isPagingEnabled = true
+        collectionView?.dataSource = self
+        view.addSubview(collectionView!)
         
     }
     
-}   // #20
+}
+
+extension HomeScreenViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeCell", for: indexPath)
+        return cell
+    }
+    
+}   // #42
---
+++ Online Edu/Tab Bar/VideoCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  VideoCollectionViewCell.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 29/08/21.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoCollectionViewCell: UICollectionViewCell {
+    
+    // Subviews
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+}   // #20
