593a810faee6b49fbdabaf848a8019143c9f0f68
Likhit Garimella <likhitgmella@gmail.com>
2021-08-28 21:04:41 +0530

    authors coll view cell sb & code

--- Online Edu/Discover/AuthorsCollectionViewCell.swift
+++ Online Edu/Discover/AuthorsCollectionViewCell.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class AuthorsCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var hashtagLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var photoImg: UIImageView!
+    @IBOutlet var bottomView: UIView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        photoImg.layer.cornerRadius = 12
+        bottomView.layer.cornerRadius = 12
+        bottomView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
+    }
     
-}   // #15
+}   // #28
