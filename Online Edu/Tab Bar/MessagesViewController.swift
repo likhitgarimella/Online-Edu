05977349663b1b28422461764bd3b61c2243cf23
Likhit Garimella <likhitgmella@gmail.com>
2021-08-29 02:47:23 +0530

    plain messages vc sb

---
+++ Online Edu/Tab Bar/MessagesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesViewController.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 28/08/21.
+//
+
+import UIKit
+
+class MessagesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // remove title for left bar button item
+        navigationController?.navigationBar.topItem?.title = ""
+        
+    }
+    
+}   // #21
