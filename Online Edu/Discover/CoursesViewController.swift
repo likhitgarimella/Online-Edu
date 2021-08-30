8798b1db3256102ddb7fcf96a7d95128e3727555
Likhit Garimella <likhitgmella@gmail.com>
2021-08-30 15:59:46 +0530

    courses vc final code @156 done

--- Online Edu/Discover/CoursesViewController.swift
+++ Online Edu/Discover/CoursesViewController.swift
@@ -6,18 +6,103 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Toast_Swift
+import MBProgressHUD
+import SDWebImage
 
 class CoursesViewController: UIViewController {
     
     @IBOutlet var coursesCollectionView: UICollectionView!
     
+    var reachability: Reachability?
+    
     var dataInfo: NSMutableArray = []
+    
+    func apiCalling() {
+        
+        do {
+            self.reachability = try Reachability.init()
+        }
+        catch {
+            print("Unable to start notifier")
+        }
+        
+        if ((reachability!.connection) != .unavailable) {
+            
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+            
+            let url = "http://3.108.14.230:3333/api/course/get"
+            print(url)
+            
+            AF.request(URL.init(string: url)!, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+                
+                print(response.request!)
+                print("1111--------------------------------1111")
+                print(response.result)
+                print("2222--------------------------------2222")
+                print(response.response!)
+                print("3333--------------------------------3333")
+                
+                switch response.result {
+                
+                case .success(let payload):
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    if let x = payload as? Dictionary<String,AnyObject> {
+                        print(x)
+                        print("4444--------------------------------4444")
+                        
+                        let resultValue = x as NSDictionary
+                        guard let message = resultValue["status"] as? String else {return}
+                        
+                        if message == "SUCCESS" {
+                            
+                            print("5555--------------------------------5555")
+                            
+                            let data = resultValue["data"] as! NSArray
+                            self.dataInfo = NSMutableArray(array: data)
+                            
+                            self.coursesCollectionView.reloadData()
+                            
+                            print("6666--------------------------------6666")
+                            
+                        } else {
+                            let alertController = UIAlertController(title: "Oops-1", message: "\(message)", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                
+                case .failure(let error):
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Oops-2", message: "App cannot be connected to the server", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    self.present(alertController, animated: true, completion: nil)
+                
+                }
+                
+            }
+            
+        } else {
+            let alertController = UIAlertController(title: "Oops-3", message: "Please check your Internet", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         coursesCollectionView.dataSource = self
         coursesCollectionView.delegate = self
+        
+        apiCalling()
         
     }
     
@@ -26,13 +111,22 @@ class CoursesViewController: UIViewController {
 extension CoursesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        /// static
-        return 30
+        return dataInfo.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CoursesCollectionViewCell", for: indexPath) as! CoursesCollectionViewCell
+        
+        let dic = dataInfo[indexPath.row] as! NSDictionary
+        if (dic["title"] as? String) != "" {
+            cell.labelOne.text! = dic["title"] as! String
+        }
+        if (dic["description"] as? String) != "" {
+            cell.labelTwo.text! = dic["description"] as! String
+        }
+        let imageUrl = dic["thumbnail"] as! String
+        cell.photoImg.sd_setImage(with: URL(string: "\(imageUrl)"), placeholderImage: UIImage(named: "trendImg"))
         
         return cell
         
@@ -58,4 +152,4 @@ extension CoursesViewController: UICollectionViewDelegateFlowLayout {
         return CGSize(width: collectionView.frame.size.width / 3 - 2, height: collectionView.frame.size.width / 3 - 2)
     }
     
-}   // #62
+}   // #156
