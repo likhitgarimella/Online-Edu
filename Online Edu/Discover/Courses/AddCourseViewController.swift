264472b66855e6da90e0424a1bb17e38b4c6b3d4
Likhit Garimella <likhitgmella@gmail.com>
2021-09-16 17:25:11 +0530

    2 files 2 assets

---
+++ Online Edu/Discover/Courses/AddCourseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddCourseViewController.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 16/09/21.
+//
+
+import UIKit
+
+class AddCourseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20
---
+++ Online Edu/Discover/Courses/CourseDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CourseDetailViewController.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 16/09/21.
+//
+
+import UIKit
+
+class CourseDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20
