c57e599653cc7fed49d7d2a5fa8d58ea2aac8319
Likhit Garimella <likhitgmella@gmail.com>
2021-09-28 12:37:53 +0530

    find friends vc sb & init code

---
+++ Online Edu/Profile/FindFriendsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FindFriendsViewController.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 28/09/21.
+//
+
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // remove title for left bar button item
+        navigationController?.navigationBar.topItem?.title = ""
+        
+    }
+    
+}   // #21
