d5bfebf6a09f87b627163ae192b2db57211ba466
Likhit Garimella <likhitgmella@gmail.com>
2021-09-29 04:45:05 +0530

    home screen implem #5

---
+++ Online Edu/Home/Player.swift
@@ -0,0 +1,24 @@
+//
+//  Player.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 29/09/21.
+//
+
+import Foundation
+import SwiftUI
+import AVKit
+
+struct Player: UIViewControllerRepresentable {
+    
+    var player: AVPlayer
+    
+    func makeUIViewController(context: Context) -> some AVPlayerViewController {
+        <#code#>
+    }
+    
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        <#code#>
+    }
+    
+}   // #25
---
+++ Online Edu/Home/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 29/09/21.
+//
+
+import Foundation
+import SwiftUI
+import AVKit
+
+struct Video: Identifiable {
+    
+    var id: Int
+    var player: AVPlayer
+    var replay: Bool
+    
+}   // #19
