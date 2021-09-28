68111b793af609fb29b0373fb1dec85c4d744c87
Likhit Garimella <likhitgmella@gmail.com>
2021-09-28 14:20:16 +0530

    preview cells vc codes ~3 done

--- Online Edu/Discover/DiscoverViewController.swift
+++ Online Edu/Discover/DiscoverViewController.swift
@@ -6,6 +6,11 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Toast_Swift
+import MBProgressHUD
+import SDWebImage
 
 class DiscoverViewController: UIViewController {
     
@@ -14,8 +19,261 @@ class DiscoverViewController: UIViewController {
     @IBOutlet var coursesPreviewCollectionView: UICollectionView!
     @IBOutlet var trendingPreviewCollectionView: UICollectionView!
     
+    var reachability: Reachability?
+    
+    var dataInfoOne: NSMutableArray = []
+    var dataInfoTwo: NSMutableArray = []
+    var dataInfoThree: NSMutableArray = []
+    
     /// images in coll view
     var items = [UIImage(named: "alex"), UIImage(named: "alex"), UIImage(named: "alex"), UIImage(named: "alex"), UIImage(named: "alex"), UIImage(named: "alex")]
+    
+    func apiCallingAuthors() {
+        
+        do {
+            self.reachability = try Reachability.init()
+        }
+        catch {
+            print("Unable to start notifier")
+        }
+        
+        if ((reachability!.connection) != .unavailable) {
+            
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+            
+            let url = "http://3.108.14.230:3333/api/author/get"
+            print(url)
+            
+            AF.request(URL.init(string: url)!, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+                
+                print(response.request!)
+                print("1111--------------------------------1111")
+                print(response.result)
+                print("2222--------------------------------2222")
+                print(response.response!)
+                print("3333--------------------------------3333")
+                
+                switch response.result {
+                
+                case .success(let payload):
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    if let x = payload as? Dictionary<String,AnyObject> {
+                        print(x)
+                        print("4444--------------------------------4444")
+                        
+                        let resultValue = x as NSDictionary
+                        // let message = resultValue["status"] as String
+                        guard let message = resultValue["status"] as? String else {return}
+                        // let code = resultValue["code"] as! String
+                        
+                        if message == "SUCCESS" {
+                            
+                            print("5555--------------------------------5555")
+                            
+                            // guard let data = resultValue["data"] as? NSDictionary else {return}
+                            let data = resultValue["data"] as! NSArray
+                            self.dataInfoOne = NSMutableArray(array: data)
+                            
+                            // guard let token = resultValue["accessToken"] as? String else {return}
+                            // let token = resultValue["accessToken"] as! String
+                            // let token = data["accessToken"] as! String
+                            // guard let userId = data["_id"] as? String else {return}
+                            // let userId = data["_id"] as! String
+                            
+                            self.authorsPreviewCollectionView.reloadData()
+                            
+                            print("6666--------------------------------6666")
+                            
+                        } else {
+                            let alertController = UIAlertController(title: "Oops-1", message: "\(message)", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                
+                case .failure(let error):
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Oops-2", message: "App cannot be connected to the server", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    self.present(alertController, animated: true, completion: nil)
+                
+                }
+                
+            }
+            
+        } else {
+            let alertController = UIAlertController(title: "Oops-3", message: "Please check your Internet", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func apiCallingCourses() {
+        
+        do {
+            self.reachability = try Reachability.init()
+        }
+        catch {
+            print("Unable to start notifier")
+        }
+        
+        if ((reachability!.connection) != .unavailable) {
+            
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+            
+            let url = "http://3.108.14.230:3333/api/course/get"
+            print(url)
+            
+            AF.request(URL.init(string: url)!, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+                
+                print(response.request!)
+                print("1111--------------------------------1111")
+                print(response.result)
+                print("2222--------------------------------2222")
+                print(response.response!)
+                print("3333--------------------------------3333")
+                
+                switch response.result {
+                
+                case .success(let payload):
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    if let x = payload as? Dictionary<String,AnyObject> {
+                        print(x)
+                        print("4444--------------------------------4444")
+                        
+                        let resultValue = x as NSDictionary
+                        guard let message = resultValue["status"] as? String else {return}
+                        
+                        if message == "SUCCESS" {
+                            
+                            print("5555--------------------------------5555")
+                            
+                            let data = resultValue["data"] as! NSArray
+                            self.dataInfoTwo = NSMutableArray(array: data)
+                            
+                            // self.realDataInfo = self.dataInfo
+                            self.coursesPreviewCollectionView.reloadData()
+                            
+                            print("6666--------------------------------6666")
+                            
+                        } else {
+                            let alertController = UIAlertController(title: "Oops-1", message: "\(message)", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                
+                case .failure(let error):
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Oops-2", message: "App cannot be connected to the server", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    self.present(alertController, animated: true, completion: nil)
+                
+                }
+                
+            }
+            
+        } else {
+            let alertController = UIAlertController(title: "Oops-3", message: "Please check your Internet", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func apiCallingTrending() {
+        
+        do {
+            self.reachability = try Reachability.init()
+        }
+        catch {
+            print("Unable to start notifier")
+        }
+        
+        if ((reachability!.connection) != .unavailable) {
+            
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+            
+            let url = "http://3.108.14.230:3333/api/trendings/get"
+            print(url)
+            
+            AF.request(URL.init(string: url)!, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+                
+                print(response.request!)
+                print("1111--------------------------------1111")
+                print(response.result)
+                print("2222--------------------------------2222")
+                print(response.response!)
+                print("3333--------------------------------3333")
+                
+                switch response.result {
+                
+                case .success(let payload):
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    if let x = payload as? Dictionary<String,AnyObject> {
+                        print(x)
+                        print("4444--------------------------------4444")
+                        
+                        let resultValue = x as NSDictionary
+                        // let message = resultValue["status"] as String
+                        guard let message = resultValue["status"] as? String else {return}
+                        // let code = resultValue["code"] as! String
+                        
+                        if message == "SUCCESS" {
+                            
+                            print("5555--------------------------------5555")
+                            
+                            // guard let data = resultValue["data"] as? NSDictionary else {return}
+                            let data = resultValue["data"] as! NSArray
+                            self.dataInfoThree = NSMutableArray(array: data)
+                            
+                            // guard let token = resultValue["accessToken"] as? String else {return}
+                            // let token = resultValue["accessToken"] as! String
+                            // let token = data["accessToken"] as! String
+                            // guard let userId = data["_id"] as? String else {return}
+                            // let userId = data["_id"] as! String
+                            
+                            self.trendingPreviewCollectionView.reloadData()
+                            
+                            print("6666--------------------------------6666")
+                            
+                        } else {
+                            let alertController = UIAlertController(title: "Oops-1", message: "\(message)", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                
+                case .failure(let error):
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Oops-2", message: "App cannot be connected to the server", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    self.present(alertController, animated: true, completion: nil)
+                
+                }
+                
+            }
+            
+        } else {
+            let alertController = UIAlertController(title: "Oops-3", message: "Please check your Internet", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +293,10 @@ class DiscoverViewController: UIViewController {
         coursesPreviewCollectionView.delegate = self
         trendingPreviewCollectionView.dataSource = self
         trendingPreviewCollectionView.delegate = self
+        
+        apiCallingAuthors()
+        apiCallingCourses()
+        apiCallingTrending()
         
     }
     
@@ -83,11 +345,44 @@ class DiscoverViewController: UIViewController {
 extension DiscoverViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if collectionView == authorsPreviewCollectionView {
+            return dataInfoOne.count
+        }
+        if collectionView == coursesPreviewCollectionView {
+            return dataInfoTwo.count
+        }
+        if collectionView == trendingPreviewCollectionView {
+            return dataInfoThree.count
+        }
         /// static
         return (items.count)
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == authorsPreviewCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AuthorsPreviewCell", for: indexPath) as! AuthorsPreviewCell
+            let dic = dataInfoOne[indexPath.row] as! NSDictionary
+            let imageUrl = dic["fileURL"] as! String
+            cell.photoImg.sd_setImage(with: URL(string: "\(imageUrl)"), placeholderImage: UIImage(named: "trendImg"))
+            return cell
+        }
+        if collectionView == coursesPreviewCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CoursesPreviewCell", for: indexPath) as! CoursesPreviewCell
+            let dic = dataInfoTwo[indexPath.row] as! NSDictionary
+            let imageUrl = dic["thumbnail"] as! String
+            cell.photoImg.sd_setImage(with: URL(string: "\(imageUrl)"), placeholderImage: UIImage(named: "courseImg"))
+            return cell
+        }
+        if collectionView == trendingPreviewCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrendingPreviewCell", for: indexPath) as! TrendingPreviewCell
+            let dic = dataInfoThree[indexPath.row] as! NSDictionary
+            let imageUrl = dic["fileURL"] as! String
+            cell.photoImg.sd_setImage(with: URL(string: "\(imageUrl)"), placeholderImage: UIImage(named: "trendImg"))
+            return cell
+        }
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LivePreviewCell", for: indexPath) as! LivePreviewCell
         cell.photoImg.image = items[indexPath.row]
@@ -95,4 +390,4 @@ extension DiscoverViewController: UICollectionViewDelegate, UICollectionViewData
         
     }
     
-}   // #99
+}   // #394
