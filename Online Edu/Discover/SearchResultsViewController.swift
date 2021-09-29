f2789e4347befbf0308d9680eb16e85a64166923
Likhit Garimella <likhitgmella@gmail.com>
2021-09-29 08:59:20 +0530

    search func implem #3

--- Online Edu/Discover/SearchResultsViewController.swift
+++ Online Edu/Discover/SearchResultsViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class SearchResultsViewController: UIViewController {
     
+    // Outlets
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchBtn: UIButton!
+    
+    func Properties() {
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: searchTextField.frame.height))
+        searchTextField.leftViewMode = .always
+        searchTextField.layer.cornerRadius = 20
+        searchTextField.layer.borderWidth = 1
+        searchTextField.layer.borderColor = UIColor.gray.cgColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        hideKeyboardWhenTappedAround()
+        Properties()
         
     }
     
-}   // #20
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+    }
+    
+}   // #37
