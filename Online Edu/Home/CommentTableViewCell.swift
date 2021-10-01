d741b17ca4358ca11b409e27c4d9d2b15e895b11
Likhit Garimella <likhitgmella@gmail.com>
2021-10-01 09:30:41 +0530

    comment screen #4

---
+++ Online Edu/Home/CommentTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentTableViewCell.swift
+//  Online Edu
+//
+//  Created by Likhit Garimella on 01/10/21.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+    
+}   // #27
